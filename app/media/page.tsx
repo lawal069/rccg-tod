f0161327701c126c731d80debd3eccdbc64c7902
lawal069 <lawaloluwaseyi069@gmail.com>
2024-03-23 16:12:13 +0100

    contact page

--- app/media/page.tsx
+++ app/media/page.tsx
@@ -1,8 +1,11 @@
+import AnimationComp from '@/components/AnimationComp'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>media page</div>
+    <div className="max-w-screen-xxl mx-auto py-12 lg:py-20">
+      <AnimationComp />
+    </div>
   )
 }
 
